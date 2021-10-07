db0030e6f43a2fdcf83270e019bd9d6a73b9da45
haley_jcmsxb6 <haley_jcmsxb6@MinePC>
2021-10-07 18:57:00 -0500

    i finished robot color chooser and crazy cat lady

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,12 +2,57 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		
+
 		//1. Create a new Robot
+		Robot mrBeast = new Robot();
+
+		while (true) {
+		String color = JOptionPane.showInputDialog("What color do you want to have Mrbeast draw with?");
+		if(color.equalsIgnoreCase("green")) { 
+			mrBeast.setPenColor(Color.green);
+		}
+		else if (color.equalsIgnoreCase("blue")) {
+		mrBeast.setPenColor(Color.blue);
+		}
+		else if (color.equalsIgnoreCase("red")){
+			mrBeast.setPenColor(Color.red);
+		}
+		else if (color.equalsIgnoreCase("pink")){
+			mrBeast.setPenColor(Color.pink);
+		}
+		else if (color.equalsIgnoreCase("yellow")){
+			mrBeast.setPenColor(Color.yellow);
+		}
+		else if (color.equalsIgnoreCase("orange")){
+			mrBeast.setPenColor(Color.orange);
+		}
+		else if (color.equalsIgnoreCase("stop")) {
+			System.exit(0);
+		}
+		else {
+			mrBeast.setRandomPenColor();
+		}
+			
+	
+		 mrBeast.setPenWidth(10);
+		mrBeast.penDown();
+		 mrBeast.setSpeed(10);
+		  mrBeast.move(100);
+		  mrBeast.turn(90);
+		  mrBeast.move(100);
+		  mrBeast.turn(90);
+		  mrBeast.move(100);
+		  mrBeast.turn(90);
+		  mrBeast.move(100);
+		  mrBeast.turn(90);
+		}
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
 		//3. Set the pen width to 10
 		//4. Ask the user what color pen they would like the robot to draw with
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,10 +3,23 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String howManyCats = JOptionPane.showInputDialog("how many cats do you have?");
+		int howManyCatsInt = Integer.parseInt(howManyCats);
+		if (howManyCatsInt>=3) {
+			JOptionPane.showMessageDialog(null, "You are a crazy cat lady.");
+		}
+		else if (howManyCatsInt>0) {
+		playVideo("https://www.youtube.com/watch?v=XyNlqQId-nk");
+		}
+		else {
+			playVideo("https://www.youtube.com/watch?v=80Qpriezo5M");
+		}
 		// 2. Convert their answer into an int
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
