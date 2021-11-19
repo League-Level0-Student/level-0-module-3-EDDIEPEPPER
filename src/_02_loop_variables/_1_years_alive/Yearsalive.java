f7d2e8650f74d235b5a543b9a1fceda7172025ed
student <student@ip-172-31-61-43.us-west-2.compute.internal>
2021-11-18 16:46:38 -0800

    i worked on years alive

---
+++ src/_02_loop_variables/_1_years_alive/Yearsalive.java
@@ -0,0 +1,10 @@
+package _02_loop_variables._1_years_alive;
+
+public class Yearsalive {
+public static void main(String[] args) {
+	for (int year = 2010; year<= 2021; year++) {
+	System.out.println(year);
+}
+}
+
+}
