c295d802ccf20eceba5b69cae383a43d3e28bdef
student <student@ip-172-31-49-154.us-west-2.compute.internal>
2021-12-30 17:05:40 -0800

    i did walk of fame

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,10 +9,22 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
+rob.setX(50);
+rob.penDown();
+rob.setSpeed(5);
 
-		// 2. Make the robot draw a star shape. Hint: angle=144.
-
-		// 3. Set the length of each line in the star to 30.
+// 2. Make the robot draw a star shape. Hint: angle=144.
+for (int i = 0; i< 10; i++) {
+	for (int q = 0; q< 5; q++) {
+rob.move(100);
+rob.turn(144);
+}
+rob.penUp();
+rob.setAngle(90);
+rob.move(50);
+rob.setAngle(0);
+rob.penDown();
+}	// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
